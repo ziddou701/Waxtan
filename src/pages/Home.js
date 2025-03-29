465da734b9db26da87a4c99ab2e21b9f9a062f19
ziddou701 <ousmane.z.konate@hotmail.com>
2025-03-29 02:09:01 -0400

    set sign out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import Cookies from "universal-cookie";
 import ChatSearch from "./components/ChatSearch";
 import Chat from "./components/Chat";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     const cookies = new Cookies();
     const Navigate = useNavigate();
     const logcook = cookies.get("auth-token");
+    const [refresh , setRefresh] = useState(false);
 
     useEffect( () => {
         // console.log(logcook);
@@ -19,7 +21,12 @@ const Home = () => {
           }else{
             Navigate('/Home');
           }
-    } , [] );
+    } , [refresh] );
+
+    const delCookies = () => {
+        cookies.set("auth-token" , '');
+        setRefresh(true);
+    };
 
     return (
         <div className="w-full max-h-screen">
@@ -33,7 +40,7 @@ const Home = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M373.1 24.97C401.2-3.147 446.8-3.147 474.9 24.97L487 37.09C515.1 65.21 515.1 110.8 487 138.9L289.8 336.2C281.1 344.8 270.4 351.1 258.6 354.5L158.6 383.1C150.2 385.5 141.2 383.1 135 376.1C128.9 370.8 126.5 361.8 128.9 353.4L157.5 253.4C160.9 241.6 167.2 230.9 175.8 222.2L373.1 24.97zM440.1 58.91C431.6 49.54 416.4 49.54 407 58.91L377.9 88L424 134.1L453.1 104.1C462.5 95.6 462.5 80.4 453.1 71.03L440.1 58.91zM203.7 266.6L186.9 325.1L245.4 308.3C249.4 307.2 252.9 305.1 255.8 302.2L390.1 168L344 121.9L209.8 256.2C206.9 259.1 204.8 262.6 203.7 266.6zM200 64C213.3 64 224 74.75 224 88C224 101.3 213.3 112 200 112H88C65.91 112 48 129.9 48 152V424C48 446.1 65.91 464 88 464H360C382.1 464 400 446.1 400 424V312C400 298.7 410.7 288 424 288C437.3 288 448 298.7 448 312V424C448 472.6 408.6 512 360 512H88C39.4 512 0 472.6 0 424V152C0 103.4 39.4 64 88 64H200z"/></svg>
                 </div>
                 {/* Settings Button */}
-                <div className="w-1.5 mr-2 ">
+                <div className="w-1.5 mr-2 " onClick={delCookies}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z"/></svg>
                 </div>
             </div>
